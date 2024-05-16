38260a503f3583dc54eca24c147fe79071f42caa
hrajabi72 <93196646+hrajabi72@users.noreply.github.com>
2024-05-16 19:44:51 -0400

    correct dictation

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 data "aws_vpc" "default"{
-  defult = true
+  default = true
 }
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
